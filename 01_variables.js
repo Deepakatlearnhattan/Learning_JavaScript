611f0160a5505d2c1d0684eb8d9bef559f4e66e2
Deepak Pandey <pandey3105deepak@gmail.com>
2024-07-15 09:38:04 +0530

    01_variables

---
+++ 01_variables.js
@@ -0,0 +1,7 @@
+const account_Id = 12345
+let account_Name = "Deepak"
+let account_Email = "Deepak@gmail.com"
+let account_Password = "2345"
+let city = "Delhi"
+
+console.table([account_Id, account_Name, account_Email, account_Password, city])
